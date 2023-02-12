87c87a497504d0accd32e44495e021806f5a5a2f
sfleveluptemp <124316260+sfleveluptemp@users.noreply.github.com>
2023-02-12 10:48:24 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,5 +20,21 @@ df = pandas.DataFrame(my_catalog)
 
 # put the first column into a list
 color_list = df[0].values.tolist()
-print(color_list)
+#print(color_list)
+
+# Let's put a pick list here so they can pick the color
+option = streamlit.selectbox('Pick a sweatsuit color or style:', list(color_list))
+
+# We'll build the image caption now, since we can
+product_caption = 'Our warm, comfortable,' + option + 'sweatsuit!'
+
+# Use the option selected to go back and get all the info from the database
+my_cur.execute("select direct_url, price, size_list, upsell_product_desc from catalog_for_website where color_or_style = '" + option + "';")
+df2 = my_cur.fetchone()
+
+streamlit.image (
+  df2[0],
+  width=400,
+  caption=product_caption
+)
 
