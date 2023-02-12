ba077753b8412cfcf9f25b9de015a86e8db484ff
sfleveluptemp <124316260+sfleveluptemp@users.noreply.github.com>
2023-02-12 10:50:58 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ color_list = df[0].values.tolist()
 option = streamlit.selectbox('Pick a sweatsuit color or style:', list(color_list))
 
 # We'll build the image caption now, since we can
-product_caption = 'Our warm, comfortable,' + option + 'sweatsuit!'
+product_caption = 'Our warm, comfortable, ' + option + ' sweatsuit!'
 
 # Use the option selected to go back and get all the info from the database
 my_cur.execute("select direct_url, price, size_list, upsell_product_desc from catalog_for_website where color_or_style = '" + option + "';")
